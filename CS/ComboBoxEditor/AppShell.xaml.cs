571982e6b38f2cb63e1789bf4f1e13fc26052164
Alexey Gorbunov <alexey.gorbunov@devexpress.com>
2022-08-09 15:07:32 +0200

    combo box

---
+++ CS/ComboBoxEditor/AppShell.xaml.cs
@@ -0,0 +1,7 @@
+﻿namespace ComboBoxEditor {
+    public partial class AppShell : Shell {
+        public AppShell() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CS/ComboBoxEditor/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using DevExpress.Maui.DataForm;
+using System.Collections;
+
+namespace ComboBoxEditor {
+    public class DepartmentInfo {
+        public int DepartmentCode { get; set; }
+        public string DepartmentName { get; set; }
+
+        public DepartmentInfo(int code, string name) {
+            DepartmentCode = code;
+            DepartmentName = name;
+        }
+    }
+
+    public class EmployeeInfo {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [DataFormComboBoxEditor(ValueMember = "DepartmentCode", DisplayMember = "DepartmentName")]
+        public int Department { get; set; }
+
+        [DataFormComboBoxEditor]
+        public string Status { get; set; }
+    }
+
+    public class ComboBoxDataProvider : IPickerSourceProvider {
+        public IEnumerable GetSource(string propertyName) {
+            if (propertyName == "Department") {
+                return new List<DepartmentInfo>() {
+                    new DepartmentInfo(0, "Sales"),
+                    new DepartmentInfo(1, "Support"),
+                    new DepartmentInfo(2, "Shipping"),
+                    new DepartmentInfo(3, "Engineering"),
+                    new DepartmentInfo(4, "Human Resources"),
+                    new DepartmentInfo(5, "Management"),
+                    new DepartmentInfo(6, "IT")
+                };
+            }
+            if (propertyName == "Status") {
+                return new List<string>() {
+                    "Salaried",
+                    "Commission",
+                    "Terminated",
+                    "On Leave"
+                };
+            }
+            return null;
+        }
+    }
+    public partial class MainPage : ContentPage {
+
+        public MainPage() {
+            InitializeComponent();
+            dataForm.DataObject = new EmployeeInfo();
+            dataForm.PickerSourceProvider = new ComboBoxDataProvider();
+        }
+    }
+}
