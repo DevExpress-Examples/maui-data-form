f8b6f77f10f322030f2bc5fee8f4109ed01c4d3c
Anton Sameishchev <anton.sameishchev@devexpress.com>
2023-11-16 21:18:11 +0400

    EditFormExample

---
+++ CS/EditFormExample/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Mail;
+using System.Runtime.CompilerServices;
+
+namespace EditFormExample {
+    public partial class MainPage : ContentPage {
+        public MainPage() {
+            InitializeComponent();
+        }
+        private void ValidateCustomerProperties(object sender, DevExpress.Maui.DataForm.DataFormPropertyValidationEventArgs e) {
+            if (e.PropertyName == "Email" && e.NewValue != null) {
+                MailAddress res;
+                if (!MailAddress.TryCreate((string)e.NewValue, out res)) {
+                    e.HasError = true;
+                    e.ErrorText = "Invalid email";
+                }
+            }
+        }
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e) {
+            dataForm.Commit();
+        }
+    }
+}
